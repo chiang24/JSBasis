a737f9421d3e00efa718661a7a734a18777f47ca
chiang24 <438085855@qq.com>
2017-07-28 11:31:44 +0800

    实现MVVM

--- MVVM/main.js
+++ MVVM/main.js
@@ -4,14 +4,32 @@ var chiang = {
         name:'chiang'
     }
 }
-Object.defineProperties(frank,'age',{
+Object.defineProperties(chiang,'age',{
     get:function(){
             return chiang._data.age
         },
-        set:function(xxx){
+    set:function(xxx){
+            $('input[name="age"]').val(xxx)
             chiang._data.age=xxx
         }
 })
-    chiang.age = 21
-    console.log(chiang.age)
-    console.dir(chiang)
+Object.defineProperties(chiang,'name',{
+    get:function(){
+            return chiang._data.name
+        },
+    set:function(xxx){
+            $('input[name="name"]').val(xxx)
+            chiang._data.name=xxx
+        }
+})
+/* HTML到内存的一个映射 */
+$('form').on('input','input[name]',function(e){
+    let $input = e.currentTarget()
+    let name = $input.attr('name')
+    let value = name.val()
+    chiang[name] = value    //修改内存里的name值
+})
+
+/* HTML初始化 */
+$('input[name="name"]').val(chiang.name)
+$('input[name="age"]').val(chiang.age)
