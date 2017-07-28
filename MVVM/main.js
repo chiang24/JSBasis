7deaa01861d11f3a87a74ed8d814b620959a5630
chiang24 <438085855@qq.com>
2017-07-28 12:00:10 +0800

    添加监听新属性的API

--- MVVM/main.js
+++ MVVM/main.js
@@ -1,29 +1,48 @@
 var chiang = {
-    _data:{
-        age:18,
-        name:'chiang'
+    _data: {
+        age: 18,
+        name: 'chiang'
     }
 }
-for(let key in chiang._data){
-Object.defineProperties(chiang,key,{
-    get:function(){
+for (let key in chiang._data) {
+    Object.defineProperties(chiang, key, {
+        get: function () {
             return chiang._data[key]
         },
-    set:function(xxx){
-            $('input[name="'+key+'"]').val(xxx)     //修改HTML
-            chiang._data[key]=xxx
+        set: function (xxx) {
+            $('input[name="' + key + '"]').val(xxx) //修改HTML
+            chiang._data[key] = xxx
         }
-})
+    })
+}
+
+/* 提供一个API可以监听新的属性 */
+function createAndSet(key, defaultValue) {
+    Object.defineProperties(chiang, key, {
+        get: function () {
+            return chiang._data[key]
+        },
+        set: function (xxx) {
+            $('input[name="' + key + '"]').val(xxx) //修改HTML
+            chiang._data[key] = xxx
+        }
+    })
+    chiang._data[key] = defaultValue
 }
 
 /* HTML到内存的一个映射 */
-$('form').on('input','input[name]',function(e){
+$('form').on('input', 'input[name]', function (e) {
     let $input = e.currentTarget()
     let name = $input.attr('name')
     let value = name.val()
-    chiang[name] = value    //修改内存里的name值
+    chiang[name] = value //修改内存里的name值
 })
+
+createAndSet('hobby', 0)
 
 /* HTML初始化 */
 $('input[name="name"]').val(chiang.name)
 $('input[name="age"]').val(chiang.age)
+$('form').on('submit', function (e) {
+    e.preventDefault()
+})
